97b0b691369a28831a90f8de8e27bf2f6ff2d5be
ivanzz.eth <ivanzz.eth@gmail.com>
2026-01-15 11:42:51 +0800

    feat: add BNB Chain (56) support using probable-go-contracts config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -17,6 +17,18 @@ type Contracts struct {
 }
 
 var (
+	// BNB Chain Mainnet (Chain ID: 56)
+	// Contract addresses from probable-go-contracts/config.go
+	_BNB_CHAIN_CONTRACTS = &Contracts{
+		Exchange:         common.HexToAddress("0xF99F5367ce708c66F0860B77B4331301A5597c86"), // CTF Exchange (from probable-go-contracts)
+		FeeModule:        common.Address{},                                                  // Not used by Probable Markets
+		NegRiskExchange:  common.Address{},                                                  // Not used by Probable Markets
+		NegRiskFeeModule: common.Address{},                                                  // Not used by Probable Markets
+		NegRiskAdapter:   common.Address{},                                                  // Not used by Probable Markets
+		Collateral:       common.HexToAddress("0x55d398326f99059fF775485246999027B3197955"), // USDT (from probable-go-contracts)
+		Conditional:      common.HexToAddress("0x364d05055614B506e2b9A287E4ac34167204cA83"), // ConditionalTokens (from probable-go-contracts)
+	}
+
 	_AMOY_CONTRACTS = &Contracts{
 		Exchange:         common.HexToAddress("0xdFE02Eb6733538f8Ea35D585af8DE5958AD99E40"),
 		FeeModule:        common.HexToAddress("0xE3f18aCc55091e2c48d883fc8C8413319d4Ab7b0"),
@@ -40,11 +52,13 @@ var (
 
 func GetContracts(chainId int64) (*Contracts, error) {
 	switch chainId {
+	case 56:
+		return _BNB_CHAIN_CONTRACTS, nil
 	case 137:
 		return _MATIC_CONTRACTS, nil
 	case 80002:
 		return _AMOY_CONTRACTS, nil
 	default:
-		return nil, fmt.Errorf("invalid chain id")
+		return nil, fmt.Errorf("invalid chain id: %d, only BNB Chain (56), Polygon (137), and Amoy (80002) are supported", chainId)
 	}
 }
