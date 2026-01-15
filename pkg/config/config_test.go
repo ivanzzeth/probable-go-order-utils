b674f062916329900366e38bf50e4d53e5565e10
ivanzz.eth <ivanzz.eth@gmail.com>
2026-01-15 11:56:13 +0800

    fix: update test to match BNB Chain config (empty NegRisk addresses)

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -12,8 +12,10 @@ func TestGetContracts(t *testing.T) {
 	var (
 		bnbChain = &Contracts{
 			Exchange:         common.HexToAddress("0xF99F5367ce708c66F0860B77B4331301A5597c86"),
-			NegRiskExchange:  common.HexToAddress("0x365fb81bd4A24D6303cd2F19c349dE6894D8d58A"),
-			NegRiskAdapter:   common.HexToAddress("0xc3Cf7c252f65E0d8D88537dF96569AE94a7F1A6E"),
+			FeeModule:        common.Address{}, // Not used by Probable Markets
+			NegRiskExchange:  common.Address{}, // Not used by Probable Markets
+			NegRiskFeeModule: common.Address{}, // Not used by Probable Markets
+			NegRiskAdapter:   common.Address{}, // Not used by Probable Markets
 			Collateral:       common.HexToAddress("0x55d398326f99059fF775485246999027B3197955"),
 			Conditional:      common.HexToAddress("0x364d05055614B506e2b9A287E4ac34167204cA83"),
 		}
@@ -23,7 +25,9 @@ func TestGetContracts(t *testing.T) {
 	assert.NotNil(t, c)
 	assert.Nil(t, err)
 	assert.True(t, bytes.Equal(c.Exchange[:], bnbChain.Exchange[:]))
+	assert.True(t, bytes.Equal(c.FeeModule[:], bnbChain.FeeModule[:]))
 	assert.True(t, bytes.Equal(c.NegRiskExchange[:], bnbChain.NegRiskExchange[:]))
+	assert.True(t, bytes.Equal(c.NegRiskFeeModule[:], bnbChain.NegRiskFeeModule[:]))
 	assert.True(t, bytes.Equal(c.NegRiskAdapter[:], bnbChain.NegRiskAdapter[:]))
 	assert.True(t, bytes.Equal(c.Collateral[:], bnbChain.Collateral[:]))
 	assert.True(t, bytes.Equal(c.Conditional[:], bnbChain.Conditional[:]))
